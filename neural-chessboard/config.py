0918d18acc18be88d562ad0b07b9b87f233b5efd
jong <jstebner007@gmail.com>
2023-02-06 10:52:02 -0500

    removed vision repos

--- neural-chessboard/config.py
+++
@@ -1,8 +0,0 @@
-from time import time
-
-NC_LAYER = 0
-NC_IMAGE = object
-NC_CLOCK = time()
-NC_DEBUG = False # True
-
-NC_CONFIG = {'layers': 3}
--- neural-chessboard/deps/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import geometry
-from . import laps
