775e75b464a4d6fa6aa13eed792a3908e4bcfdc9
jong <jstebner007@gmail.com>
2023-02-08 16:11:05 -0500

    added a really shitty version of delay ig

--- botfish.py
+++ botfish.py
@@ -4,9 +4,23 @@ from getopt import getopt
 from stockfish import Stockfish
 import pygame
 from pygame.locals import *
+import time
+import random
 
 class Botfish:
-    def __init__(self, engine, lvl, clr):
+    def __init__(self, engine: str, lvl: int, clr: chr):
+        """_summary_
+
+        Args:
+            engine (str): path of stockfish engine
+            lvl (int):  difficulty of the robot
+                            0: ez
+                            1: mid
+                            2: ultra nightmare mode
+            clr (char): color bot is playing:
+                            'W': white
+                            'B': black
+        """
         self.sf = Stockfish(
             path=engine,
             parameters={
@@ -17,9 +31,10 @@ class Botfish:
                 }
             )
         self.clr = clr
+        self.delay_s = (2-lvl) * 5
             
-    def process_move(self, move: str) -> tuple[int, str]:
-        """update board state with opponent move and return stockfish move
+    def process_player_move(self, move: str) -> tuple[int, str]:
+        """update board state with player move and return stockfish move
 
         Args:
             move (str): move performed: "{from_coord}{to_coord}"; ex: "a2a3"
@@ -30,15 +45,15 @@ class Botfish:
                     0: EXIT
                     1: ERROR
                     2: OKAY
-                str: response string (move or error message)             
+                str: response string (move or error message)
         """
 
         # NOTE: process input
         # stopwords
-        if any([stopword in move for stopword in ('exit', 'quit', 'stop', 'end')]): 
+        if any([stopword in move for stopword in ('exit', 'quit', 'stop', 'end', 'LET ME OUT OF THIS CHAIR YOU IDIOT')]): 
             return (0, 'stopword received')
         
-        # legal move check    
+        # legal move check
         if not self.sf.is_move_correct(move):
             return (1,'you idiot')
 
@@ -51,17 +66,18 @@ class Botfish:
         return (2, move)
     
     # TODO: all this jawn
-    def send_move(self, move: str) -> None: # REVIEW: return type of this
+    def send_bot_move(self, move: str) -> None: # REVIEW: return type of this
         """send chess move to manip node
 
         Args:
             move (str): move performed: "{from_coord}{to_coord}"; ex: "a2a3"
         """
         print("sending move:", move)
+        time.sleep(random.normalvariate(mu=self.delay_s, sigma=1)) # TODO: make this not suck ass or smthn
         pass
     
     # TODO: all this jawn as well
-    def get_move(self) -> str:
+    def get_player_move(self) -> str:
         """retrieve move made by player
 
         Returns:
@@ -91,13 +107,13 @@ class Botfish:
 
         if self.clr == 'W':
             move = self.sf.get_best_move()
-            self.send_move(move)
+            self.send_bot_move(move)
             self.sf.make_moves_from_current_position([move])
         
         move = ''
         running = True
         while running:
-            # ui stuffs
+            # TODO: ui stuffs 
             screen.fill((0,0,0))
             avatar = pygame.Rect(0, 0, 20, 20)
             pygame.draw.rect(screen,(64,31,255),avatar)
@@ -106,17 +122,17 @@ class Botfish:
                 pass
             
             print(self.sf.get_board_visual()) # TODO: rmv this
-            # NOTE: player move
 
-            move = self.get_move()
-            status, response = self.process_move(move)
+            move = self.get_player_move()
+            status, response = self.process_player_move(move)
             if status == 0: # exit
                 print("stopword received")
                 exit()
             if status == 1: # error
                 print(response)
-            if status == 2:
-                self.send_move(move)
+            if status == 2: # okay
+                
+                self.send_bot_move(move)
                 print(self.sf.get_board_visual()) # TODO: rmv this
             
             
