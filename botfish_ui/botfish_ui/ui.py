f6ae3998d44ef28adcd1941189933e84e825568f
jong <jstebner007@gmail.com>
2023-02-13 13:29:32 -0500

    starting cleaning fr this time

--- botfish_ui/botfish_ui/ui.py
+++ botfish_ui/botfish_ui/ui.py
@@ -9,11 +9,26 @@ from multiprocessing import Queue
 import pygame
 from pygame.locals import *
 
+# utils
 DIM = (1920//2, 1080//2) # rmv //2 for final
 PERIOD_s = 1/32 # 32 UpS
+SMOLFONT = pygame.font.SysFont('monospace', 10)
+BEEGFONT = pygame.font.SysFont('monospace', 30)
+CLRS = { # TODO: make these better and more
+    'white': (255,255,255),
+    'black': (0,0,0),
+    'gray': (100,100,100)
+}
+def draw_text(screen, text, color, x, y, is_big = False):
+    font = BEEGFONT if is_big else SMOLFONT
+    text_obj = font.render(text, True, color)
+    text_rect = text_obj.get_rect()
+    text_rect.topleft = (x, y)
+    screen.blit(text_obj, text_rect)
+def draw_rect(screen, color, x, y, dims):
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, *dims))
 
 class UIController(Node):
-    
     def __init__(self):
         pygame.init()
         super().__init__('ui_controller')
@@ -31,6 +46,7 @@ class UIController(Node):
             'ui_ping',
             10
         )
+        # add anotha pub guy for setup stuffs here
         self.gamestate_sub = self.create_subscription(
             String,
             'gamestate',
@@ -48,12 +64,7 @@ class UIController(Node):
             self.update
         )
 
-
-    def update(self):
-        # TODO: make this look good
-        self.screen.fill((0,0,0))
-        
-        # event listener # TODO: maybe make this its own func or smthn
+    def event_listener(self):
         for event in pygame.event.get():
             if event.type in [QUIT, K_ESCAPE]:
                 # cleanup here
@@ -72,15 +83,23 @@ class UIController(Node):
                         self.i += 1
                         ping = String()
                         ping.data = f'ping {self.i}'
-                        print(ping.data)
+                        # print(ping.data)
                         self.ui_ping_pub.publish(ping)
                         self.is_player_turn = False
+    
+    def update(self):
+        # TODO: make this look good
+        self.screen.fill(CLRS['black'])
+        
+        # event listener # TODO: maybe make this its own func or smthn
+        self.event_listener()
 
         # TODO: the white half of the timer i think         
-        pygame.draw.rect(
-            self.screen, 
-            (255,255,255,255),
-            pygame.Rect(0, 0, DIM[0]//2, DIM[1])
+        draw_rect(
+            screen=self.screen,
+            color=CLRS['white'],
+            x=0, y=0,
+            dims=(DIM[0]//2, DIM[1])
         )
         
         # TODO: mfin timers n whatnot
