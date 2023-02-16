12edadb4333fb97b8fc652404d83557faa2e2c09
Jordan Krasan <jkrasan@umich.edu>
2023-02-15 21:05:43 -0500

    Fixed node creation within itself

--- botfish_manipulation/include/manipulation/manipulation.hpp
+++ botfish_manipulation/include/manipulation/manipulation.hpp
@@ -5,6 +5,7 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <std_msgs/msg/string.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
+#include <vector>
 
 namespace manip {
 
@@ -33,13 +34,21 @@ namespace manip {
 
         moveit::planning_interface::MoveGroupInterface *move_group_interface = nullptr;
 
-        /// @brief Calculates the xyz/quaternion that the end effector of the robot needs to go to
+        /// @brief Callback for moves from the engine
         /// @param msg string msg containing the move outputted by the engine, format will be startingLocationEndingLocation ex: e2e4
-        void calculate_trajectory(std_msgs::msg::String::SharedPtr msg);
+        void move_cb(std_msgs::msg::String::SharedPtr msg);
+
+        /// @brief Calculates the xyz/quaternion that the end effector of the robot needs to go to and moves arm to that location
+        /// @param location cell to move arm to
+        void actuate(cell_location location);
+
+        /// @breif grabs piece from board using grippers
+        /// @param position whether were grasping or releasing, true = grasping, false = releasing
+        void grab(bool position);
 
         /// @brief Converts string moves to xy locations
-        /// @param move string move recieved in the calculate_trajectory callback
+        /// @param move string move recieved in the move_cb callback
         /// @return tuple of two cell_location structs each containing two doubles for both x and y location
-        std::tuple<cell_location, cell_location> parse_move(std::string move);
+        std::vector<cell_location> parse_move(std::string move);
     };
 }
--- botfish_manipulation/src/manipulation_node.cpp
+++ botfish_manipulation/src/manipulation_node.cpp
@@ -7,32 +7,60 @@ manip::Manipulation::Manipulation(rclcpp::NodeOptions options) : Node("manipulat
     _end_effector_link = this->declare_parameter("end_effector", "right_arm_6_link");
     _reference_link = this->declare_parameter("reference_link", "right_arm_podent_link");
     _moveit_group = this->declare_parameter("move_group", "right_arm");
-    _grab_height = this->declare_parameter("grab_height", 0.1);
-    _goal_tolerance = this->declare_parameter("goal_tolerance", 0.5);
+    _grab_height = this->declare_parameter("grab_height", 0.352);
+    _goal_tolerance = this->declare_parameter("goal_tolerance", 0.0125);
 
     _engine_move_sub = this->create_subscription<std_msgs::msg::String>(
-            "/engine_move", 10, std::bind(&Manipulation::calculate_trajectory, this, std::placeholders::_1));
+            "/engine_move", 10, std::bind(&Manipulation::move_cb, this, std::placeholders::_1));
 
-    auto const logger = rclcpp::get_logger("manipulation");
-
-    auto manip_node = std::make_shared<manip::Manipulation>(options);
-
-    (*move_group_interface) = moveit::planning_interface::MoveGroupInterface(manip_node, _moveit_group);
+    (*move_group_interface) = moveit::planning_interface::MoveGroupInterface(static_cast<const SharedPtr>(this),
+                                                                             _moveit_group);
 
     move_group_interface->setPoseReferenceFrame(_reference_link);
     move_group_interface->setEndEffector(_end_effector_link);
     move_group_interface->setGoalTolerance(_goal_tolerance);
 }
 
-void manip::Manipulation::calculate_trajectory(std_msgs::msg::String::SharedPtr msg) {
-    std::tuple<manip::cell_location, manip::cell_location> parsed_moves = parse_move(msg->data);
-    _target_pose.position.x = this->_starting_position.position.x + std::get<0>(parsed_moves).x_dist;
-    _target_pose.position.y = this->_starting_position.position.y + std::get<1>(parsed_moves).y_dist;
-    _target_pose.position.z = this->_starting_position.position.z;
-    _target_pose.orientation = this->_hand_orientation;
+void manip::Manipulation::move_cb(std_msgs::msg::String::SharedPtr msg) {
+    std::vector<manip::cell_location> parsed_moves = parse_move(msg->data);
+    for (auto i: parsed_moves) {
+        actuate(i);
+        grab(true);
+    }
 }
 
-std::tuple<manip::cell_location, manip::cell_location> manip::Manipulation::parse_move(std::string move) {
-    std::tuple<manip::cell_location, manip::cell_location> parsed_moves{};
+std::vector<manip::cell_location> manip::Manipulation::parse_move(std::string move) {
+    std::vector<manip::cell_location> parsed_moves;
+
+
     return parsed_moves;
+}
+
+void manip::Manipulation::actuate(manip::cell_location location) {
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+
+    //Calculate cell location to move to
+    _target_pose.position.x = this->_starting_position.position.x + location.x_dist;
+    _target_pose.position.y = this->_starting_position.position.y + location.y_dist;
+    _target_pose.position.z = this->_starting_position.position.z;
+    _target_pose.orientation = this->_hand_orientation;
+    move_group_interface->setPoseTarget(_target_pose);
+
+    //Attempt to plan to that position
+    RCLCPP_INFO(this->get_logger(), "Planning movement...");
+    auto const plan_ok = static_cast<bool>(move_group_interface->plan(msg));
+    if (!plan_ok) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to plan!!!");
+    } else {
+        //Execute our calculated plan
+        RCLCPP_INFO(this->get_logger(), "Executing movement...");
+        auto const exec_ok = static_cast<bool>(move_group_interface->execute(msg));
+        if (!exec_ok) {
+            RCLCPP_ERROR(this->get_logger(), "Execute failed!!!");
+        }
+    }
+}
+
+void manip::Manipulation::grab(bool position) {
+
 }
